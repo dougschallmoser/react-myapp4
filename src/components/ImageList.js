3f5f8610a7025ad6c9602affd6c876b3c3ea72c4
Doug Schallmoser <dougschallmoser@mgail.com>
2020-10-31 20:05:44 -0700

    add functionality to display images in a grid and size div containers according to image height

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,12 @@
+import './ImageList.css'
 import React from 'react';
+import ImageCard from './ImageCard';
 
 const ImageList = ({ images }) => {
-  const theImages = images.map(({ id, description, urls }) => {
-    return <img key={id} src={urls.regular} alt={description} />
+  const theImages = images.map(image => {
+    return <ImageCard key={image.id} image={image} />
   })
-return <div>{theImages}</div>
+return <div className="image-list">{theImages}</div>
 }
 
 export default ImageList
